42247bea4cd1e8adf3b6087e932e5c4299925694
scoundrel-lyc <48325980+scoundrel-lyc@users.noreply.github.com>
2021-04-28 20:20:14 +0800

    updates
    :>

---
+++ src/03.数组中重复的数字.cpp
@@ -0,0 +1,65 @@
+﻿/*
+    剑指 Offer 03. 数组中重复的数字
+    找出数组中重复的数字。
+    在一个长度为 n 的数组 nums 里的所有数字都在 0～n-1 的范围内。
+    数组中某些数字是重复的，但不知道有几个数字重复了，也不知道每个数字重复了几次。
+    请找出数组中任意一个重复的数字。
+
+    输入：
+    [2, 3, 1, 0, 2, 5, 3]
+    输出：2 或 3
+*/
+
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+
+
+using namespace std;
+
+class Solution
+{
+public:
+    int findRepeatNumber(vector<int>& nums) {
+        unordered_map<int, int>hash;
+        for (int i = 0; i < nums.size(); i++) {
+            if (hash.count(nums[i]) != 0) {
+                return nums[i];
+            }
+            hash[nums[i]]++;
+        }
+        return 0;
+    }
+};
+
+// ACM 模式 输入一个一维vector
+template<typename T>
+void fcin(vector<T>& v) {
+    for (int i = 0; i < v.size(); ++i) {
+        cin >> v[i];
+    }
+}
+
+
+int main()
+{
+    cout << "***********************************************" << endl;
+    cout << "剑指 Offer 03. 数组中重复的数字" << endl;
+    cout << "***********************************************" << endl;
+
+
+    //输入
+    //m为输入测试矩阵的个数
+    int m;
+    cout << "输入测试矩阵的个数:";
+    cin >> m;cout << endl;
+    cout << "输入测试数据:";
+    vector<int>T(m,0);
+    fcin(T);
+    Solution a;
+    int res = a.findRepeatNumber(T);
+    cout << "答案为：" << res << endl;
+    return 0;
+
+}
+
---
+++ src/24.反转链表.cpp
@@ -0,0 +1,87 @@
+﻿/*
+    剑指 Offer 24. 反转链表
+    定义一个函数，输入一个链表的头节点，反转该链表并输出反转后链表的头节点。
+    
+    输入: 1->2->3->4->5->NULL
+    输出: 5->4->3->2->1->NULL
+*/
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode(int x) : val(x), next(nullptr) {};
+};
+
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* pre = nullptr;
+        ListNode* cur = head;
+        while (cur) {
+            ListNode* fod = cur->next;
+            cur->next = pre;
+            pre = cur;
+            cur = fod;
+        }
+        return pre;
+    }
+};
+
+void fcin(vector<int>& v, int m) {
+    while (m) {
+        int n;
+        cin >> n;
+        v.push_back(n);
+        m--;
+    }
+}
+
+//建立链表，返回头结点
+ListNode* creatList(vector<int>& v) {
+    ListNode* SB = new ListNode(0);
+    ListNode* pre = SB;
+    int i = 0;
+    while (i < v.size()) {
+        ListNode* cur = new ListNode(v[i]);
+        pre->next = cur;
+        pre = cur;
+        i++;
+    }
+    return SB->next;
+}
+
+int main()
+{
+    cout << "***********************************************" << endl;
+    cout << "剑指 Offer 24. 反转链表" << endl;
+    cout << "***********************************************" << endl;
+
+    int m;
+    cout << "输入链表元素个数:" ;
+    cin >> m;
+    cout << endl;
+
+    vector<int>T;
+
+    cout << "输入链表元素：";
+    fcin(T, m);
+    cout << endl;
+
+    //建立链表
+    ListNode* head = creatList(T);
+    Solution a;
+    ListNode* res = a.reverseList(head);
+    cout << "反转后的链表：";
+    while (res) {
+        cout << res->val << " ";
+        res = res->next;
+    }
+    cout << endl;
+
+    return 0;
+}
+
